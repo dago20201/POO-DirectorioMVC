9fa729bcdba09cc5be8dd09398d188cc487ff7e4
202402145-crypto <202402145@umh.edu.hn>
2025-09-11 22:07:02 -0600

    claseGrado

--- DirectorioMVC/obj/Debug/net8.0/POO-DirectorioMVC.AssemblyInfo.cs
+++ DirectorioMVC/obj/Debug/net8.0/POO-DirectorioMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("POO-DirectorioMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9209b8707b5c45d8ff2023a30269d9cdc849c59")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93f268c987ada11609ba93eff7ec71638377674a")]
 [assembly: System.Reflection.AssemblyProductAttribute("POO-DirectorioMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("POO-DirectorioMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
