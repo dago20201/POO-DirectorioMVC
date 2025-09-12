a2053c948067f07a505d7e846a8b9953ff04ba8f
dzroberto72 <dzroberto72@gmail.com>
2025-09-11 22:38:28 -0600

    Actualiar

---
+++ DirectorioMVC/Models/Marcador.cs
@@ -0,0 +1,36 @@
+﻿namespace DirectorioMVC.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    namespace Directorio.Clases
+    {
+        public class Marcador
+        {
+            private double costoHora { get; set; }
+            private double horasTrabajadas { get; set; }
+            private double salario { get; set; }
+
+            public Marcador()
+            {
+                this.costoHora = 50;
+            }
+            public double getSalario(double p_horasTrabajadas)
+            {
+
+                this.horasTrabajadas = p_horasTrabajadas;
+                this.salario = this.horasTrabajadas * this.costoHora;
+
+                return this.salario;
+            }
+
+
+
+
+        }
+    }
+
+}
--- DirectorioMVC/obj/Debug/net8.0/DirectorioMVC.AssemblyInfo.cs
+++ DirectorioMVC/obj/Debug/net8.0/DirectorioMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DirectorioMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d7a6771eab7b44db13c395a2f07fa55c36bfbbe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+204fb90f22c9ee231edd231a195c41bbe0f51f7f")]
 [assembly: System.Reflection.AssemblyProductAttribute("DirectorioMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DirectorioMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
