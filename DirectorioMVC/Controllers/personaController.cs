be149465beb986bd00426554c5731973d0876654
dzroberto72 <dzroberto72@gmail.com>
2025-09-17 17:27:41 -0600

    actualizacion

--- DirectorioMVC/Controllers/personaController.cs
+++ DirectorioMVC/Controllers/personaController.cs
@@ -1,9 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using POO_DirectorioMVC.Models;
 
 namespace POO_DirectorioMVC.Controllers
 {
-    public class personaController : Controller
+    public class PersonaController : Controller
     {
+       Persona myPersona = new Persona("Jose");
+
+
+
+
         public IActionResult Index()
         {
             return View();
---
+++ DirectorioMVC/Models/Persona.cs
@@ -0,0 +1,53 @@
+﻿namespace POO_DirectorioMVC.Models
+{
+    public class Persona
+    {
+        private string numeroid { set; get; }
+
+        private string nombres { set; get; }
+
+        public string apellidos { set; get; }
+        public string direccion { set; get; }
+        public int edad { set; get; }
+        public DateTime fechaNacimiento { set; get; }
+        public string telefono { set; get; }
+        public string celular { set; get; }
+        public string email { set; get; }
+        public Persona()
+        {
+            this.nombres = "Jose";
+        }
+
+        public Persona(string p_nombre)
+        {
+            this.nombres = p_nombre;
+        }
+        public string getNombre()
+        {
+            return this.nombres;
+        }
+        public void setNombre(string p_nombres)
+        {
+            this.nombres = p_nombres;
+        }
+
+        public void setApellidos(string p_apellidos)
+        {
+            this.apellidos = p_apellidos;
+        }
+        public string getNombreCompleto()
+        {
+            return this.nombres + "" + this.apellidos;
+        }
+
+        public string getNombreCompleto(string p_nombre, string p_apellido)
+        {
+            this.nombres = p_nombre;
+            this.apellidos = p_apellido;
+
+            return this.nombres + "" + this.apellidos;
+
+        }
+
+    }
+}
